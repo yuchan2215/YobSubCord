b038d722ff4c290223b49b00b04a3328d4245b48
Miyayu <yuchan2215@gmail.com>
2022-01-13 14:39:48 +0900

    Hello Worldをした

---
+++ src/main/kotlin/xyz/miyayu/yobsub/yobsubcord/MainController.kt
@@ -0,0 +1,12 @@
+package xyz.miyayu.yobsub.yobsubcord
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class MainController {
+    @RequestMapping("/")
+    fun test() :String{
+        return "Hello World"
+    }
+}
