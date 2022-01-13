7630d005b043feb97d948b126caea4779af735ee
Miyayu <yuchan2215@gmail.com>
2022-01-13 12:26:58 +0900

    first commit

---
+++ src/main/kotlin/xyz/miyayu/yobsub/yobsubcord/YobSubCordApplication.kt
@@ -0,0 +1,11 @@
+package xyz.miyayu.yobsub.yobsubcord
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YobSubCordApplication
+
+fun main(args: Array<String>) {
+    runApplication<YobSubCordApplication>(*args)
+}
