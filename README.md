9336861f9622eb855fdb1ca6bf2f5ecb15bc003c
Miyayu <yuchan2215@gmail.com>
2022-01-13 15:57:06 +0900

    ライセンス関係の追加

---
+++ README.md
@@ -0,0 +1,17 @@
+# LICENSES
+ - jackson-module-kotlin
+   - [Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0)
+ - jackson-module-kotlin
+   - [Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0)
+ - kotlin-reflect
+   - [Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0)
+ - kotlin-stdlib-jdk8
+   - [Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0)
+ - spring-boot-starter-test
+   - [Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0)
+ - gradle
+   - [Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0)
+ - dependency-management-plugin
+   - [Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0)
+ - JDK17
+   - [Oracle No-Fee Terms and Conditions (NFTC)](https://www.oracle.com/downloads/licenses/no-fee-license.html)
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.6.2"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.6.10"
-    kotlin("plugin.spring") version "1.6.10"
+    id("org.springframework.boot") version "2.6.2" //LICENSE OK
+    id("io.spring.dependency-management") version "1.0.11.RELEASE" //LICENSE OK
+    kotlin("jvm") version "1.6.10" //LICENSE OK
+    kotlin("plugin.spring") version "1.6.10" //LICENSE OK
 }
 
 group = "xyz.miyayu.yobsub"
@@ -16,11 +16,11 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-web") //LICENSE OK
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin") //LICENSE OK
+    implementation("org.jetbrains.kotlin:kotlin-reflect") //LICENSE OK
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8") //LICENSE OK
+    testImplementation("org.springframework.boot:spring-boot-starter-test") //LICENSE OK
 }
 
 tasks.withType<KotlinCompile> {
