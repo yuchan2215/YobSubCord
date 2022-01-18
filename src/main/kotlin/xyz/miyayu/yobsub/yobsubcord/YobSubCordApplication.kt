f82b34127d6d7ae788be7615cd4599905c5ed227
Miyayu <yuchan2215@gmail.com>
2022-01-18 17:35:50 +0900

    slf4jのloggerに対応した

--- src/main/kotlin/xyz/miyayu/yobsub/yobsubcord/YobSubCordApplication.kt
+++ src/main/kotlin/xyz/miyayu/yobsub/yobsubcord/YobSubCordApplication.kt
@@ -2,12 +2,14 @@ package xyz.miyayu.yobsub.yobsubcord
 
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import javax.security.auth.login.LoginException
 
 @SpringBootApplication
 class YobSubCordApplication
+val logger = LoggerFactory.getLogger(YobSubCordApplication::class.java)
 var jda:JDA? = null
 fun main(args: Array<String>) {
     runApplication<YobSubCordApplication>(*args)
@@ -16,15 +18,16 @@ fun main(args: Array<String>) {
 }
 
 fun loadEnv() {
-    println("環境変数を読み込みます...")
-    println("DISCORDTOKEN:\t%s".format(EnvWrapper.DISCORD_TOKEN))
+    logger.info("環境変数を読み込みます...")
+    logger.info("DISCORDTOKEN:\t%s".format(EnvWrapper.DISCORD_TOKEN))
 }
 fun loadDiscord(){
     try {
         jda = JDABuilder.createDefault(EnvWrapper.DISCORD_TOKEN).build()
     }catch(e:LoginException){
-        println("DiscordBotのログインに失敗しました")
+        logger.error(e.stackTraceToString())
+        logger.error("DiscordBotのログインに失敗しました")
     }catch(e:Exception) {
-        e.stackTrace
+        logger.error(e.message)
     }
 }
