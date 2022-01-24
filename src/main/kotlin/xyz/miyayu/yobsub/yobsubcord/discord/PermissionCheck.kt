f827c9dcfbd1a269095f04ba3aa7b8c5c0669ea0
Miyayu <yuchan2215@gmail.com>
2022-01-24 10:40:37 +0900

    権限を持っているユーザーかどうか確認する関数を作成

---
+++ src/main/kotlin/xyz/miyayu/yobsub/yobsubcord/discord/PermissionCheck.kt
@@ -0,0 +1,14 @@
+package xyz.miyayu.yobsub.yobsubcord.discord
+
+import net.dv8tion.jda.api.entities.Member
+import xyz.miyayu.yobsub.yobsubcord.EnvWrapper
+
+fun checkPermission(member:Member):Boolean{
+    //Adminロールを持っているかどうか
+    if(member.roles.contains(
+            member.jda.getRoleById(EnvWrapper.ADMIN_ROLE))
+    )return true
+
+    //デバッグユーザーかどうか
+    return member.id == EnvWrapper.DEBUG_USER
+}
