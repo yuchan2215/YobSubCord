44ab2ca2049f5508278b5f1114ba9ab2da095846
Miyayu <yuchan2215@gmail.com>
2022-01-24 10:47:26 +0900

    ボタン作成コマンドで文字列チェック処理を追加

--- src/main/kotlin/xyz/miyayu/yobsub/yobsubcord/discord/commands/MakeButton.kt
+++ src/main/kotlin/xyz/miyayu/yobsub/yobsubcord/discord/commands/MakeButton.kt
@@ -6,8 +6,10 @@ import xyz.miyayu.yobsub.yobsubcord.discord.checkPermission
 
 class MakeButton :ListenerAdapter(){
     override fun onMessageReceived(event: MessageReceivedEvent) {
+        //コマンドかどうか
+        if(event.message.contentRaw != "tb.mb")return
         //権限を持っているかどうか
         if(event.member?.let{checkPermission(it)} == false)return
-        
+
     }
 }
