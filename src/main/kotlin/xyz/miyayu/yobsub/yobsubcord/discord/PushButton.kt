546eab5c0783172a48f82ac0ac3ed76d688f0dd5
Miyayu <yuchan2215@gmail.com>
2022-01-24 11:38:36 +0900

    ライブアラートの権限付与処理を追加

--- src/main/kotlin/xyz/miyayu/yobsub/yobsubcord/discord/PushButton.kt
+++ src/main/kotlin/xyz/miyayu/yobsub/yobsubcord/discord/PushButton.kt
@@ -2,6 +2,7 @@ package xyz.miyayu.yobsub.yobsubcord.discord
 
 import net.dv8tion.jda.api.events.interaction.ButtonClickEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import xyz.miyayu.yobsub.yobsubcord.EnvWrapper
 
 class PushButton:ListenerAdapter() {
     override fun onButtonClick(event: ButtonClickEvent) {
@@ -19,7 +20,17 @@ class PushButton:ListenerAdapter() {
         }
     }
     fun liveAlert(event: ButtonClickEvent){
-
+        val role = event.jda.getRoleById(EnvWrapper.ALERT_ROLE)
+        try{
+            if(event.member!!.roles.contains(role!!)){
+                event.reply("すでに購読しています！").setEphemeral(true).queue()
+            }else {
+                event.guild!!.addRoleToMember(event.member!!, role).queue()
+                event.reply("処理が正常に完了しました！購読ありがとうございます。").setEphemeral(true).queue()
+            }
+        }catch(e:Exception){
+            event.reply("エラーが発生したためロールを付与することができませんでした。運営に報告してください。\tエラー内容：" + e.message).queue()
+        }
     }
     fun liveAlertDm(event: ButtonClickEvent){
 
