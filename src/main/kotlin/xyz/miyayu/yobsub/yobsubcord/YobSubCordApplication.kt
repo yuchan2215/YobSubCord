d6bb9e55e8604d5779538d9376af684470fc6fb5
Miyayu <yuchan2215@gmail.com>
2022-02-01 23:27:08 +0900

    add: サブスクライブする処理を追加

--- src/main/kotlin/xyz/miyayu/yobsub/yobsubcord/YobSubCordApplication.kt
+++ src/main/kotlin/xyz/miyayu/yobsub/yobsubcord/YobSubCordApplication.kt
@@ -9,6 +9,7 @@ import xyz.miyayu.yobsub.yobsubcord.discord.JDAWrapper
 import xyz.miyayu.yobsub.yobsubcord.discord.PushButton
 import xyz.miyayu.yobsub.yobsubcord.discord.commands.Eval
 import xyz.miyayu.yobsub.yobsubcord.discord.commands.MakeButton
+import xyz.miyayu.yobsub.yobsubcord.pubsub.subscribe
 
 @SpringBootApplication
 class YobSubCordApplication
@@ -22,6 +23,7 @@ fun main(args: Array<String>) {
     runApplication<YobSubCordApplication>(*args)
     loadEnv()
     loadDiscord()
+    subscribe()
 }
 fun loadEnv() {
     logger.info("環境変数を読み込みます...")
