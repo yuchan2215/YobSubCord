b9b3db2acaa5b1542a1fd6efccffbde52d8db99e
Miyayu <yuchan2215@gmail.com>
2022-02-02 00:10:43 +0900

    add: DOMを分析して分岐させた

--- src/main/kotlin/xyz/miyayu/yobsub/yobsubcord/pubsub/Notification.kt
+++ src/main/kotlin/xyz/miyayu/yobsub/yobsubcord/pubsub/Notification.kt
@@ -4,6 +4,10 @@ import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import org.w3c.dom.Node
+import org.xml.sax.InputSource
+import java.io.StringReader
+import javax.xml.parsers.DocumentBuilderFactory
 
 @RestController
 class Notification {
@@ -11,6 +15,36 @@ class Notification {
     val notificationLogger = LoggerFactory.getLogger("xyz.miyayu.yobsub.notlog")
     @PostMapping("hub")
     fun postNotification(@RequestBody body: String){
-        logger.info(body)
+        val inputSource = InputSource(StringReader(body))
+
+        val dbf = DocumentBuilderFactory.newInstance()
+        val db = dbf.newDocumentBuilder()
+        val document = db.parse(inputSource)
+        document.documentElement.normalize()
+        val feed = document.documentElement
+
+        //処理を走ったかどうか確認する
+        var runned = false
+
+        val nodeList = feed.childNodes
+        for(i in 0 until nodeList.length){
+            val node = nodeList.item(i)
+            //投稿なら
+            if(node.nodeName.equals("entry") && node.nodeType == Node.ELEMENT_NODE){
+                logger.info("-投稿-")
+                runned = true
+                break
+            }
+            //削除なら
+            if(node.nodeName.equals("at:deleted-entry") && node.nodeType == Node.ELEMENT_NODE){
+                logger.info("-削除-")
+                runned = true
+                break
+            }
+        }
+        if(!runned){
+            logger.warn("不明なDOM")
+            logger.info(body)
+        }
     }
 }
