7bea67b0928c90f4f9d558f5ca1c0503c2def57a
Miyayu <yuchan2215@gmail.com>
2022-02-02 01:19:32 +0900

    add: 通知の処理を作成

--- src/main/kotlin/xyz/miyayu/yobsub/yobsubcord/pubsub/Notification.kt
+++ src/main/kotlin/xyz/miyayu/yobsub/yobsubcord/pubsub/Notification.kt
@@ -1,11 +1,15 @@
 package xyz.miyayu.yobsub.yobsubcord.pubsub
 
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import org.w3c.dom.Element
 import org.w3c.dom.Node
 import org.xml.sax.InputSource
+import xyz.miyayu.yobsub.yobsubcord.EnvWrapper
 import xyz.miyayu.yobsub.yobsubcord.getChildNodeMaps
 import java.io.StringReader
 import javax.xml.parsers.DocumentBuilderFactory
@@ -15,7 +19,7 @@ class Notification {
     val logger = LoggerFactory.getLogger(Notification::class.java)
     val notificationLogger = LoggerFactory.getLogger("xyz.miyayu.yobsub.notlog")
     @PostMapping("hub")
-    fun postNotification(@RequestBody body: String){
+    fun postNotification(@RequestBody body: String): ResponseEntity<String>{
         val inputSource = InputSource(StringReader(body))
 
         val dbf = DocumentBuilderFactory.newInstance()
@@ -24,25 +28,48 @@ class Notification {
         document.documentElement.normalize()
         val feed = document.documentElement
 
-        //処理を走ったかどうか確認する
-        var runned = false
-
         val nodeMap = getChildNodeMaps(feed)
         
         //投稿なら
         if(nodeMap.containsKey("entry")){
             logger.info("-投稿-")
+            val entryMap = getChildNodeMaps(nodeMap["entry"]!!)
+            val videoId = entryMap["yt:videoId"]!!.textContent
+            val channelId = entryMap["yt:channelId"]!!.textContent
 
+            if(!isApprovalChannel(channelId)){
+                logger.warn("許可されていないチャンネル")
+                logger.info(body)
+                return ResponseEntity("Not Approval Channel",HttpStatus.BAD_REQUEST)
+            }
         }
         //削除なら
         else if(nodeMap.containsKey("at:deleted-entry")){
             logger.info("-削除-")
+            val deletedEntry = nodeMap["at:deleted-entry"]!!
+            val ref = (deletedEntry as Element).getAttribute("ref")
+            val videoId = ref.split(":")[2]
+            print("removed: $videoId")
+
 
         }
         //それ以外なら
         else{
             logger.warn("不明なDOM")
             logger.info(body)
+            return ResponseEntity("Unknown data",HttpStatus.BAD_REQUEST)
         }
+        return ResponseEntity("",HttpStatus.OK)
+    }
+
+    /**
+     *
+     * 環境変数にチャンネルidが含まれているか
+     *
+     * @param id チャンネルid
+     * @return return 環境変数に含まれているか
+     */
+    fun isApprovalChannel(id:String):Boolean{
+        return EnvWrapper.YTCHANNELS.contains(id)
     }
 }
