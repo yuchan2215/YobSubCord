8e2b637a5509dcfba63e3a53b10cd8f555026184
Miyayu <yuchan2215@gmail.com>
2022-02-01 23:55:11 +0900

    add: hubへのPOST時の関数を追加

---
+++ src/main/kotlin/xyz/miyayu/yobsub/yobsubcord/pubsub/Notification.kt
@@ -0,0 +1,15 @@
+package xyz.miyayu.yobsub.yobsubcord.pubsub
+
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class Notification {
+    val logger = LoggerFactory.getLogger(Notification::class.java)
+    @PostMapping("hub")
+    fun postNotification(@RequestBody body: String){
+        logger.info(body)
+    }
+}
