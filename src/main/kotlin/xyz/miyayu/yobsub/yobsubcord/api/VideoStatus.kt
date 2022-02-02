4a4adfec0d9692a43a6e7c1adaa369d0297d8d87
Miyayu <yuchan2215@gmail.com>
2022-02-03 00:10:20 +0900

    add: VideoStatusにデータベース用のInt値を追加

--- src/main/kotlin/xyz/miyayu/yobsub/yobsubcord/api/VideoStatus.kt
+++ src/main/kotlin/xyz/miyayu/yobsub/yobsubcord/api/VideoStatus.kt
@@ -1,7 +1,7 @@
 package xyz.miyayu.yobsub.yobsubcord.api
 
-enum class VideoStatus {
-    VIDEO,
-    PRE_LIVE,
-    NOW_LIVE
+enum class VideoStatus(val dataValue:Int) {
+    VIDEO(0),
+    PRE_LIVE(1),
+    NOW_LIVE(2)
 }
