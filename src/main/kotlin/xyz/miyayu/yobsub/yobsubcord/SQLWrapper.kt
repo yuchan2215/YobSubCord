e45274931785a4b6a4085b5c984b590500e7c4b7
Miyayu <yuchan2215@gmail.com>
2022-02-03 01:05:34 +0900

    fix: CREATEなのでQueryからUpdateに変更

--- src/main/kotlin/xyz/miyayu/yobsub/yobsubcord/SQLWrapper.kt
+++ src/main/kotlin/xyz/miyayu/yobsub/yobsubcord/SQLWrapper.kt
@@ -14,7 +14,7 @@ fun getSQLConnection():Connection{
 fun createTables(){
     try {
         getSQLConnection().use {
-            it.createStatement().executeQuery("CREATE TABLE IF NOT EXISTS videos(videoId TEXT PRIMARY KEY,channelId TEXT,videoTitle TEXT,videoStatus INT,lastLook BLOB)")
+            it.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS videos(videoId TEXT PRIMARY KEY,channelId TEXT,videoTitle TEXT,videoStatus INT,lastLook BLOB)")
         }
     }catch(e:Exception){
         throw e
